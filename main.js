f6c54236f94f3b3a88c86973c249480d1a98d426
Connor Hudson <technoboy10@users.noreply.github.com>
2016-12-02 08:04:22 -0700

    This might work. ¯\_(ツ)_/¯

--- main.js
+++ main.js
@@ -15,57 +15,51 @@ if (request.status === 200) {
 var curators;
 var count = 0;
 function getUnread(page){
-  var commentList;
-  var ownerList;
-  var curatorList;
-  var topList;
-  //Grab comments
-  var commentReq = new XMLHttpRequest();
-  commentReq.onreadystatechange = function(){
-    if (commentReq.readyState = 4){
-      var container = document.implementation.createHTMLDocument().documentElement;
-      container.innerHTML = commentReq.responseText;
-      commentList = Array.from(container.querySelectorAll('.top-level-reply')).reverse().filter( //Get only comments with links in them
-        function(comment){
-          var c = comment.querySelector(".comment > .info > .content");
-          if (c){
-            return c.innerHTML.match(/projects\/[0-9]+/) != null;
+  return new Promise(function(resolve, reject) {
+    var commentList;
+    var ownerList;
+    var curatorList;
+    var topList;
+    //Grab comments
+    var commentReq = new XMLHttpRequest();
+    commentReq.open("GET", "https://crossorigin.me/https://scratch.mit.edu/site-api/comments/gallery/" + studioid + "/?page=" + page , true);
+    commentReq.onload = function(){
+        var container = document.implementation.createHTMLDocument().documentElement;
+        container.innerHTML = commentReq.responseText;
+        commentList = Array.from(container.querySelectorAll('.top-level-reply')).reverse().filter( //Get only comments with links in them
+          function(comment){
+            var c = comment.querySelector(".comment > .info > .content");
+            if (c){
+              return c.innerHTML.match(/projects\/[0-9]+/) != null;
+            }
+          }
+        ).map(
+          function(comment){
+            return comment.querySelector('.replies');
+          }
+        );
+      //Parse through array
+      var tempCount = commentList.length;
+      var lastReply = -1;
+      for (i = 0; i < commentList.length; i++){
+        var replyList = commentList[i].querySelectorAll('.reply > .comment'); //Get all comments
+        for (j = 0; j < replyList.length; j++){ //go through replies
+          if(curators.indexOf(replyList[j].querySelector(".info > .name > a").innerHTML) != -1){ //pick comment by a curator
+            lastReply = i;
+            tempCount--;
+            break;
           }
         }
-      ).map(
-        function(comment){
-          return comment.querySelector('.replies');
-        }
-      )
-
-    }
-  }
-  commentReq.open("GET", "https://crossorigin.me/https://scratch.mit.edu/site-api/comments/gallery/" + studioid + "/?page=" + page , true);
-  commentReq.setRequestHeader("Origin","https://technoboy10.tk");
-  commentReq.send(null);
-
-  //document.querySelectorAll('li > .avatar')[0].getAttribute('data-id')
-
-  //Parse through array
-  var tempCount = commentList.length;
-  var lastReply = -1;
-  for (i = 0; i < commentList.length; i++){
-    var replyList = commentList[i].querySelectorAll('.reply > .comment'); //Get all comments
-    for (j = 0; j < replyList.length; j++){ //go through replies
-      if(curators.indexOf(replyList[j].querySelector(".info > .name > a").innerHTML) != -1){ //pick comment by a curator
-        lastReply = i;
-        tempCount--;
-        break;
+      }
+      if (lastReply == commentList.length-1){
+        resolve(false); //All comments on page have been checked, so this should return the first comment of the next page (commentList[0]) if the next page *isn't* done
+      } else {
+        count += tempCount;
+        resolve(commentList[lastReply+1].getAttribute('data-thread')); // last unread link
       }
     }
-  }
-  if (lastReply == commentList.length-1){
-    return false; //All comments on page have been checked, so this should return the first comment of the next page (commentList[0]) if the next page *isn't* done
-  } else {
-    count += tempCount;
-    return commentList[lastReply+1].getAttribute('data-thread'); // last unread link
-  }
-
+    commentReq.send();
+  });
 }
 
 function formatLink(lastCommentId){
@@ -111,21 +105,19 @@ function getCurators(){
     console.log(curators);
 }
 
-function nextLink(){
-  var previousLink;
-  var link = true;
-  var page = 1;
-  count = 0;
-  getCurators();
-  while (link){
-    previousLink = link;
-    link = getUnread(page);
-    console.log(page + ' and ' + link);
-    page++;
-  }
-  if (previousLink != true){
-    return formatLink(previousLink);
-  } else {
-    return "https://scratch.mit.edu/studios/" + studioid + "/comments/";
-  }
+function nextLink(prevLink, page){
+  return new Promise(function(resolve, reject) {
+    var previousLink;
+    var page = 1;
+    count = 0;
+    getCurators();
+    getUnread(page).then(function (res) {
+      if (!prevLink && res){
+        resolve(formatLink(previousLink));
+      } else {
+        console.log(page + ' and ' + link);
+        nextLink(res, page + 1).then(function (link) {resolve(link)});
+     }
+   });
+ });
 }
